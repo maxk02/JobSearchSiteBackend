3bee653dfbe6610fe0487b49409ed3eaed5e4942
Maksym Kharchenko <charchenkomax.new@protonmail.com>
2025-11-26 13:44:09 +0100

    Checked Locations API

--- src/JobSearchSiteBackend.API/Controllers/Locations/Dtos/GetLocationsRequest.cs
+++ src/JobSearchSiteBackend.API/Controllers/Locations/Dtos/GetLocationsRequest.cs
@@ -1,6 +1,3 @@
-﻿using Ardalis.Result;
-using JobSearchSiteBackend.Core.Domains._Shared.UseCaseStructure;
-
-namespace JobSearchSiteBackend.API.Controllers.Locations.Dtos;
+﻿namespace JobSearchSiteBackend.API.Controllers.Locations.Dtos;
 
 public record GetLocationsRequest(long CountryId, string Query);
