08277bcbf77cf289018eb488b411c8861122a4da
Maksym Kharchenko <charchenkomax.new@protonmail.com>
2026-01-03 20:01:44 +0100

    Added JobSalaryInfoNameUpdates migration

--- src/JobSearchSiteBackend.Infrastructure/Persistence/EfCore/Migrations/20260103190051_JobSalaryInfoNameUpdates.Designer.cs
+++ src/JobSearchSiteBackend.Infrastructure/Persistence/EfCore/Migrations/20260103190051_JobSalaryInfoNameUpdates.Designer.cs
@@ -4,6 +4,7 @@ using JobSearchSiteBackend.Core.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace JobSearchSiteBackend.Infrastructure.Persistence.EfCore.Migrations
 {
     [DbContext(typeof(MainDataContext))]
-    partial class MainDataContextModelSnapshot : ModelSnapshot
+    [Migration("20260103190051_JobSalaryInfoNameUpdates")]
+    partial class JobSalaryInfoNameUpdates
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
